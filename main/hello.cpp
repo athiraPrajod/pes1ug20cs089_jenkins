69bd91d0050d1e395db00010c8243e125bb81908
athiraPrajod <athiraprajod1@gmail.com>
2023-02-16 22:19:02 +0530

    added Helloworld.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,5 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run the container\n";
-  cout << "Changed the hello.cpp file\n";
   return 0;
 }
